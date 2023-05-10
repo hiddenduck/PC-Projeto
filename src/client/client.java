fdbed7b851366f9fdbfcd3ff25166db92e484fdf
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2023-05-10 19:10:31 +0100

    Some Intellij helper

--- src/client/client.java
+++ src/client/client.java
@@ -13,7 +13,6 @@ public class client {
             ConnectionManager cm = ConnectionManager.start(socket);
             
             // new Thread();
-
         } catch(Exception e){
             e.printStackTrace();
             System.exit(0);
