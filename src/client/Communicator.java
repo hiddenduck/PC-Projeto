3550cc4a5982c6d8f714bed7d8100609e0a06c49
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-05-24 21:51:23 +0100

    Update Communicator.java

--- src/client/Communicator.java
+++ src/client/Communicator.java
@@ -58,7 +58,7 @@ class CommunicatorBox extends Communicator{
                     StringBuilder temp = new StringBuilder();
                     boolean minus = true;
                     if(box.charAt(0)=='+'){
-                        for(i=1; i<box.length() && box.charAt(i)!='-'; i++){
+                        for(i=2; i<box.length() && box.charAt(i)!='-'; i++){
                             if(box.charAt(i)!=':'){
                                 temp.append(box.charAt(i));
                             }else{
