6bad305f2b43afe8f544ed5c0391a2158603aabb
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2023-05-25 23:44:26 +0100

    Update Processing.java

--- src/client/Processing.java
+++ src/client/Processing.java
@@ -212,7 +212,7 @@ private void topMenu(){
   fill(206, 235, 251);
   triangle(width*0.05f, 0, 0, height*0.025f, width*0.05f, height*0.05f);
   textSize(width*0.1f);
-  text("Page " + this.topMaxLimit/8, width*0.5f, height*0.025f);
+  text("Page " + (this.topMaxLimit/8+1), width*0.5f, height*0.025f);
   textSize(width*0.05f);
   for(int i=this.topMinLimit; i<topMaxLimit; i++){
     text(this.topNames.get(i)+ " " + this.topLevels.get(i), width*0.5f, height*0.2f+height*0.06f*i);
@@ -270,9 +270,9 @@ private void deleteMenu(){
   triangle(width*0.05f, 0, 0, height*0.025f, width*0.05f, height*0.05f);
   fill(112,128,144);
   rect(width*0.45f, height*0.65f, width*0.1f, height*0.1f);
-  textSize(width*0.1f);
+  textSize(width*0.05f);
   fill(255,160,122);
-  text(message, width*0.5f, height*0.80f);
+  text(message, width*0.3f, height*0.80f);
 }
 
 private void game() throws IOException{
